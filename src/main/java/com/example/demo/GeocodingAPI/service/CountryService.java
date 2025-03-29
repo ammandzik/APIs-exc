ab4decb95af38ab439c7b454439dd3c4d0eeb885
ammandzik <preisner.amanda@gmail.com>
2025-03-29 17:41:51 +0100

    Geocoding API connection

--- src/main/java/com/example/demo/GeocodingAPI/service/CountryService.java
+++ src/main/java/com/example/demo/GeocodingAPI/service/CountryService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.GeocodingAPI.service;
 
 import com.example.demo.model.CityResponse;
 import org.springframework.beans.factory.annotation.Autowired;
